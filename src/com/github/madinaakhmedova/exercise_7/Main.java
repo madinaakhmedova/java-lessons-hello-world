1078858c2be5c8b206ee19fef35f17fb76bbd0c2
madinaakhmedova <akhmedovamadina11@gmail.com>
2021-04-18 23:04:49 +0300

    Exercise 7; circle for

---
+++ src/com/github/madinaakhmedova/exercise_7/Main.java
@@ -0,0 +1,26 @@
+package com.github.madinaakhmedova.exercise_7;
+
+import java.util.Scanner;
+
+public class Main {
+
+    static int x = 123;
+    static int y = 434;
+    static int z = 7;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Выберите размер массива");
+        int size = scanner.nextInt();
+
+        int[] ar = new int[size];
+        for (int i = 0; i < ar.length; i++) {
+            System.out.println("Введите число");
+            int val = scanner.nextInt();
+            if (val == x || val == y || val == z) {
+                System.out.println("Данное значение имеется в константах");
+            }
+            ar[i] = val;
+        }
+    }
+}
