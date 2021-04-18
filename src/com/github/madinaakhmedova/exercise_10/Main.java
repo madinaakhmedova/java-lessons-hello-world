82edd411bf97ea5a3acdd15e2b0da9a3c786ba03
madinaakhmedova <akhmedovamadina11@gmail.com>
2021-04-18 23:26:54 +0300

    Exercise 10; matrix

---
+++ src/com/github/madinaakhmedova/exercise_10/Main.java
@@ -0,0 +1,30 @@
+package com.github.madinaakhmedova.exercise_10;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Выберите размерность n матрицы");
+        int n = scanner.nextInt();
+        System.out.println("Выберите размерность m матрицы");
+        int m = scanner.nextInt();
+
+        int[][] matrix = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                System.out.printf("Выберите элемент матрицы (%d, %d)\n", i, j);
+                int val = scanner.nextInt();
+                matrix[i][j] = val;
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                System.out.printf("%d ", matrix[i][j] *3);
+            }
+            System.out.print("\n");
+            break;
+        }
+    }
+}
